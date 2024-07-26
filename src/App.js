e21ddf84e40e847e94772b09915908e0e1502e38
Ben Rehmann <brehmann@me.com>
2024-07-26 09:07:02 -0500

    neutron star

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import MapLoader from './components/MapLoader/MapLoader';
 import ExplorerMap from './components/ExplorerMap/ExplorerMap';
 import UphillBox from './components/UphillBox/UphillBox';
+import NeutronStar from './components/NeutronStar/NeutronStar';
 
 function App() {
   return (
     <div className="App">
 
       <MapLoader />
-      <ExplorerMap />
+      {/* <ExplorerMap /> */}
       <UphillBox /> 
+      <NeutronStar />
 
     </div>
   );
---
+++ src/components/NeutronStar/NeutronStar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NeutronStar.css';
+
+
+const NeutronStar = () => {
+  return (
+    <div className='neutronContainer'>
+      <div className='neutronStar'></div>
+    </div>
+  )
+}
+
+export default NeutronStar
