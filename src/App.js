17a555900c8ce5404e464c25be6da58fd1dfe88a
Ben Rehmann <brehmann@me.com>
2024-07-26 09:24:48 -0500

    bat beat

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MapLoader from './components/MapLoader/MapLoader';
 import ExplorerMap from './components/ExplorerMap/ExplorerMap';
 import UphillBox from './components/UphillBox/UphillBox';
 import NeutronStar from './components/NeutronStar/NeutronStar';
+import BatLoader from './components/BatLoader/BatLoader';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       {/* <ExplorerMap /> */}
       <UphillBox /> 
       <NeutronStar />
+      <BatLoader /> 
 
     </div>
   );
---
+++ src/components/BatLoader/BatLoader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BatLoader.css';
+
+const BatLoader = () => {
+    return (
+        <div className='batContainer'>
+            <div className='batLoader'>
+                <div className='batBeat'></div>
+            </div>
+        </div>
+    )
+}
+
+export default BatLoader
